612525624686a17fe9a958a4f6af817866efd280
Ali Emre Gök <81609259+aliemregk@users.noreply.github.com>
2022-06-09 17:13:29 +0300

    Initial commit.

---
+++ DataAccess/Concrete/EntityFramework/ECommerceContext.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class ECommerceContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=(localdb)\MSSQLLocalDB;Database=SeniorDB;Trusted_Connection=true");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<MainCategory> MainCategories { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+    }
+}
---
+++ Entities/Concrete/MainCategory.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class MainCategory : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PhotoId { get; set; }
+    }
+
+    public class Order : IEntity
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public bool IsDelivered { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
